d37af89ce985688f7176fcd7d7256cb609b9c51e
Zakariae <zakariaelhichem@gmail.com>
2022-10-09 15:04:39 -0400

    Run a first test with Firestore

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import Tutorial from './tutorial.model';
+
+import { TutorialService } from './tutorial.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'gestion-stock';
+  tutorial: Tutorial = new Tutorial();
+  submitted = false;
+
+  constructor(private tutorialService: TutorialService) { }
+
+  ngOnInit(): void {
+  }
+
+  saveTutorial(): void {
+    this.tutorialService.create(this.tutorial).then(() => {
+      console.log('Created new item successfully!');
+      this.submitted = true;
+    });
+  }
+
+  newTutorial(): void {
+    this.submitted = false;
+    this.tutorial = new Tutorial();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+import { environment } from '../environments/environment';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -10,7 +14,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule, // for firestore
   ],
   providers: [],
   bootstrap: [AppComponent]
