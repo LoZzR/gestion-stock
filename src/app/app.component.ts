5fbdfe8484560c96d9a50d7e88ba961c7c13c4a8
Zakariae <zakariaelhichem@gmail.com>
2022-10-10 14:16:05 -0400

    Add primeng library and clean project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,4 @@
 import { Component } from '@angular/core';
-import Tutorial from './tutorial.model';
-
-import { TutorialService } from './tutorial.service';
 
 @Component({
   selector: 'app-root',
@@ -9,23 +6,5 @@ import { TutorialService } from './tutorial.service';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  tutorial: Tutorial = new Tutorial();
-  submitted = false;
-
-  constructor(private tutorialService: TutorialService) { }
-
-  ngOnInit(): void {
-  }
-
-  saveTutorial(): void {
-    this.tutorialService.create(this.tutorial).then(() => {
-      console.log('Created new item successfully!');
-      this.submitted = true;
-    });
-  }
-
-  newTutorial(): void {
-    this.submitted = false;
-    this.tutorial = new Tutorial();
-  }
+  title = 'gestion-stock';
 }
