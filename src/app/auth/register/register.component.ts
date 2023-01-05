24bef82da6e78106aa932f572edd51bf4eedf5ea
LoZzR <zakariaelhichem@gmail.com>
2023-01-04 22:42:34 -0500

    Add validation to registration form

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -6,7 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
-  form: any = {};
+
+  passwordValue = null;
+  passwordConfirmationValue = null;
 
   constructor() { }
 
