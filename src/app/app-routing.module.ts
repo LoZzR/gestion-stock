49ab760928ca66edde6a1c6d8fa8f22ba894754f
LoZzR <zakariaelhichem@gmail.com>
2023-01-06 23:48:15 -0500

    Add welcome component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './auth/login/login.component';
 import { RegisterComponent } from './auth/register/register.component';
 import { HomeComponent } from './home/home.component';
+import { WelcomeComponent } from './welcome/welcome.component';
 
 const routes: Routes = [
   { path: "", redirectTo: "/home", pathMatch: "full" },
   { path: "home", component: HomeComponent },
   { path: "signin", component: LoginComponent },
-  { path: "signup", component: RegisterComponent }
+  { path: "signup", component: RegisterComponent },
+  { path: "welcome", component: WelcomeComponent }
 ];
 
 @NgModule({
