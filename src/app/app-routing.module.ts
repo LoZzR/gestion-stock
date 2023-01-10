910f6905940001dec0c9e9ab30e496760cb0ce45
LoZzR <zakariaelhichem@gmail.com>
2023-01-09 23:05:06 -0500

    Implement first menu navigation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ArticleListComponent } from './articles/article-list/article-list.component';
 import { LoginComponent } from './auth/login/login.component';
 import { RegisterComponent } from './auth/register/register.component';
+import { EntrepriseComponent } from './entreprise/entreprise.component';
 import { HomeComponent } from './home/home.component';
 import { MainComponent } from './shared/main/main.component';
+import { StoreListComponent } from './stores/store-list/store-list.component';
 import { WelcomeComponent } from './welcome/welcome.component';
 
 const routes: Routes = [
@@ -11,8 +14,12 @@ const routes: Routes = [
   { path: "home", component: HomeComponent },
   { path: "signin", component: LoginComponent },
   { path: "signup", component: RegisterComponent },
-  { path: "welcome", component: WelcomeComponent },
-  { path: "main", component: MainComponent }
+  { path: "main", component: MainComponent, children: [
+    { path: "", component: WelcomeComponent },
+    { path: "entreprise", component: EntrepriseComponent },
+    { path: "magasins", component: StoreListComponent },
+    { path: "articles", component: ArticleListComponent }
+  ]}
 ];
 
 @NgModule({
