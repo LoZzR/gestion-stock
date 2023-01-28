816d3c0bf94c93e88e474ca116e0c297ea5394be
LoZzR <zakariaelhichem@gmail.com>
2023-01-27 23:53:24 -0500

    Correct username in welcome component

--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -1,18 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { AuthService } from '../shared/services/auth.service';
+import { User } from '../shared/services/user.model';
 
 @Component({
   selector: 'app-welcome',
   templateUrl: './welcome.component.html',
   styleUrls: ['./welcome.component.css']
 })
-export class WelcomeComponent implements OnInit {
+export class WelcomeComponent implements OnInit, OnDestroy {
+  private userSub: Subscription = null!;
+  userName: string = '';
 
-  userName: string = "";
-
-  constructor() { }
+  constructor(private authService: AuthService) { }
 
   ngOnInit(): void {
-    this.userName = "TEST";
+    this.userSub = this.authService.user.subscribe((user: User) => {
+      if(!!user) this.userName = user.firstname + ' ' + user.lastname
+    });
+  }
+
+  onLogout() {
+    this.authService.logout();
+  }
+
+  ngOnDestroy() {
+    this.userSub.unsubscribe();
   }
 
 }
