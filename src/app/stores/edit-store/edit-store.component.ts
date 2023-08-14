6036968096de2671f58d405e739264f743812471
Zakariae EL HICHEM <zakariaelhichem@gmail.com>
2023-08-13 22:22:58 -0400

    Implement cancel adding store

--- src/app/stores/edit-store/edit-store.component.ts
+++ src/app/stores/edit-store/edit-store.component.ts
@@ -49,8 +49,8 @@ export class EditStoreComponent implements OnInit, OnDestroy {
     form.reset();
   }
 
-  cancel(form: NgForm){
-    form.reset();
+  cancel(){
+    this.router.navigate(['/main/magasins']);
   }
 
   previousPage() {
